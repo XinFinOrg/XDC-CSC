21955083179e3cd10191ad542d328aff186015b6
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2023-10-12 15:13:26 +0400

    Update ProxyGateway.js

--- test/ProxyGateway.js
+++ test/ProxyGateway.js
@@ -106,15 +106,15 @@ describe("proxyGateway", () => {
       await proxyGateway.upgrade(fullProxyAddress, proxyTest.address);
       const afterUpgradeResult = await fullProxy.getHeaderByNumber(1);
 
-      expect(fullResult[0]).to.eq(
+      expect(fullResult.hash).to.eq(
         "0x684d18e0081cbe82cab66173647eaf2b078413da5f79a1082a5228314c23ae15"
       );
-      expect(fullResult[1]).to.eq(1);
+      expect(fullResult.number).to.eq(1);
 
-      expect(afterUpgradeResult[0]).to.eq(
+      expect(afterUpgradeResult.hash).to.eq(
         "0x0000000000000000000000000000000000000000000000000000000000000666"
       );
-      expect(afterUpgradeResult[1]).to.eq(666);
+      expect(afterUpgradeResult.number).to.eq(666);
     });
   });
 });
