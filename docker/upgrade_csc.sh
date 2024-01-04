d8db8b120059ac36f92c8b8fb5ce909ec07f58d3
wanwiset25 <wanwiset25@gmail.com>
2024-01-04 14:55:29 +0400

    Merge commit '73ff6772f4c4857fb58bbaeb18c3a4f13b086bc1'

--- docker/upgrade_csc.sh
+++ docker/upgrade_csc.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /app
 cp /app/config/deployment.config.json /app/deployment.config.json
-
-if [[ -n "$PARENTCHAIN_WALLET_PK" ]]; then
-  PRIVATE_KEY=${PARENTCHAIN_WALLET_PK}
+if [[ -n "$PARENTNET_WALLET_PK" ]]; then
+  PRIVATE_KEY=${PARENTNET_WALLET_PK}
 fi
 
 
@@ -12,10 +11,10 @@ if [[ -z "$PRIVATE_KEY" ]]; then
 fi
 
 if [[ -z "$PRIVATE_KEY" ]]; then
-  if [[ -n "$PARENTCHAIN_WALLET_PK" ]]; then
-    PRIVATE_KEY=${PARENTCHAIN_WALLET_PK}
+  if [[ -n "$PARENTNET_WALLET_PK" ]]; then
+    PRIVATE_KEY=${PARENTNET_WALLET_PK}
   else
-    echo "PARENTCHAIN_WALLET_PK or PRIVATE_KEY not set"
+    echo "PARENTNET_WALLET_PK or PRIVATE_KEY not set"
     exit 1
   fi
 fi
@@ -25,11 +24,11 @@ if [[ ${PRIVATE_KEY::2} == "0x"  ]]; then
 fi
 echo "PRIVATE_KEY=${PRIVATE_KEY}" > .env 
 
-if [[ -z "$PARENTCHAIN_URL" ]]; then
-  echo "PARENTCHAIN_URL not specified"
+if [[ -z "$PARENTNET_URL" ]]; then
+  echo "PARENTNET_URL not specified"
   exit 1
 else
-  cat network.config.json | sed -e "s@\"xdcparentnet\".*@\"xdcparentnet\": \"$PARENTCHAIN_URL\",@" > temp.json
+  cat network.config.json | sed -e "s@\"xdcparentnet\".*@\"xdcparentnet\": \"$PARENTNET_URL\",@" > temp.json
   mv temp.json network.config.json
 fi
 
