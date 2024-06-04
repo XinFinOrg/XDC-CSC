a791542cc6c7b9ce407e4d0793f3e4740cb8dacb
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-04 13:43:09 +0400

    change some commit

--- scripts/utils/xdcnet.js
+++ scripts/utils/xdcnet.js
@@ -1,6 +1,5 @@
 const fetch = require("node-fetch").default;
 
-
 function base64ToHex(base64String) {
   // Step 1: Decode base64 string to binary data
   var binaryString = atob(base64String);
@@ -25,7 +24,7 @@ async function data(url, number) {
 
   let data0;
   try {
-    console.error("connecting to parentnet at url:", url,);
+    console.error("connecting to network at url:", url);
     const block0res = await fetch(url, {
       method: "POST",
       body: JSON.stringify(block0),
@@ -35,15 +34,11 @@ async function data(url, number) {
     data0 = await block0res.json();
   } catch (e) {
     console.error(e, "\n");
-    throw Error(
-      "Fetch remote subnet node data error , pls check the subnet node status"
-    );
+    throw Error("Fetch remote node data error , pls check the node status");
   }
 
   if (!data0["result"]["Committed"]) {
-    console.error(
-      "remote subnet node block data 0 or block 1 is not committed"
-    );
+    console.error("remote node block " + number + " is not committed");
     return;
   }
   const data0Encoded = "0x" + base64ToHex(data0["result"]["EncodedRLP"]);
